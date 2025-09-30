e9b55af7df717c60deeec68f66c73f5a778007c4
shain126 <shubanish.126@gmail.com>
2025-09-30 22:04:48 +0530

    Built .o's for loader.s, kernel.cpp and finally build mykernel.bin. The OS is now ready to be deployed. Simply put the mykernel.bin file in the grub menu entry. And OS should be loaded when selecting the OS from the grub loading menu.

--- kernel.cpp
+++ kernel.cpp
@@ -1,6 +1,17 @@
-#include <cstdio>
+void printf(char* str)
+{
+    short* video_memory = (short*)0xb8000;
+    static int i = 0;
+    while(*str != '\0')
+    {
+        video_memory[i] = (video_memory[i] & 0xFF00) | str[i];
+        //video_memory[i++] = *str++;
+        //video_memory[i++] = 0x07;
+    }
+}
 
-void kernalMain(){
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+{
     printf("Hello, World!");
     while(1);
-}
+} 
--- loader.s
+++ loader.s
@@ -1,9 +1,21 @@
+.set MAGIC, 0x1badb002
+.set FLAGS, (1<<0 | 1 <<1)
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+.section .multiboot
+    .long MAGIC
+    .long FLAGS
+    .long CHECKSUM
+
+
 .section .text
 .extern kernelMain
 .global leader
 
 loader:
     mov $kernel_stack, %esp
+    push %eax
+    push %ebx
     call kernelMain
 _stop:
     cli
